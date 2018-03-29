b2404ba821d25f89954c6eebdf448b4060707c7f
RowyDonker <Rowydonker@live.nl>
2018-03-29 10:01:12 +0200

    Style Veranderd

--- view/head.php
+++ view/head.php
@@ -1,9 +1,5 @@
 <head>
  <script src="javascript/scripts.js"></script>
-
-
-
- 
  <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
--- view/home.php
+++ view/home.php
@@ -1,13 +1,8 @@
- <!DOCTYPE html>
-<html>
-<head>
- <script src="scripts.js"></script>
- <link rel="stylesheet" type="text/css" href="style.css">
-</head>
 <body>
  <ul id="NAV">
  <h1 id="Titel">Gerechten</h1>
   <li style="float:right"><a class="active" href="#about">Admin</a></li>
+   <li style="float:right"><a href="../Files/bestellijst-2017-1(1).pdf" download="Bestellijst">Handmatig invullen?</a></li>
 </ul> 
 
 
@@ -26,7 +21,7 @@
     <div class="CONTENT">
     <div class="GerechtTekst">
     <h2 class="GerechtNaam">Biefstuk</h2>
-    <p class="Beschrijving">Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...Beschrijving...</p>
+    <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
     </div>
      </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
@@ -47,9 +42,36 @@
     <div class="GerechtTekst">
     <h2 class="GerechtNaam">Biefstuk</h2>
     <p class="Beschrijving">Beschrijving...</p>
+    </div>
+     </div>
+    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
+</div>
+<div class="FoodSelection">
+    <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
+    <div class="CONTENT">
+    <div class="GerechtTekst">
+    <h2 class="GerechtNaam">Biefstuk</h2>
+    <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
+    </div>
+     </div>
+    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
+</div><div class="FoodSelection">
+    <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
+    <div class="CONTENT">
+    <div class="GerechtTekst">
+    <h2 class="GerechtNaam">Biefstuk</h2>
+    <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
+    </div>
+     </div>
+    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
+</div><div class="FoodSelection">
+    <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
+    <div class="CONTENT">
+    <div class="GerechtTekst">
+    <h2 class="GerechtNaam">Biefstuk</h2>
+    <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
     </div>
      </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
 </div>
 </body>
-</html> 
