a8ba4f38fb05aca84dcaba3cbd17a020d448dc00
RowyDonker <Rowydonker@live.nl>
2018-03-29 10:27:53 +0200

    Aanpassingen

--- view/home.php
+++ view/home.php
@@ -5,15 +5,6 @@
    <li style="float:right"><a href="../Files/bestellijst-2017-1(1).pdf" download="Bestellijst">Handmatig invullen?</a></li>
 </ul> 
 
-
-
-
-
-
-
-
-
-
 <div class="FoodSelection">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
     <div class="CONTENT">
@@ -24,26 +15,7 @@
      </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
 </div>
-<div class="FoodSelection">
-    <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
-    <div class="CONTENT">
-    <div class="GerechtTekst">
-    <h2 class="GerechtNaam">Biefstuk</h2>
-    <p class="Beschrijving">Beschrijving...</p>
-    </div>
-     </div>
-    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
-</div>
-<div class="FoodSelection">
-    <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
-    <div class="CONTENT">
-    <div class="GerechtTekst">
-    <h2 class="GerechtNaam">Biefstuk</h2>
-    <p class="Beschrijving">Beschrijving...</p>
-    </div>
-     </div>
-    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
-</div>
+
 <div class="FoodSelection">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
     <div class="CONTENT">
