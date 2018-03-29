18cf1aaa893385431c8f667dc9f972aceba0c13d
joey steffens <joeysteffens@quicknet.nl>
2018-03-29 10:25:41 +0200

    making names more clear

--- view/home.php
+++ view/home.php
@@ -1,9 +1,5 @@
 <body>
- <ul id="NAV">
- <h1 id="Titel">Gerechten</h1>
-  <li style="float:right"><a class="active" href="#about">Admin</a></li>
-   <li style="float:right"><a href="../Files/bestellijst-2017-1(1).pdf" download="Bestellijst">Handmatig invullen?</a></li>
-</ul> 
+
 
 
 
