dddcce3f3575ee8de1ea3ff0ed40048ea6659e56
joey steffens <joeysteffens@quicknet.nl>
2018-04-19 10:54:47 +0200

    js for shopping basket

--- view/AmsterdamseBroodjes.php
+++ view/AmsterdamseBroodjes.php
@@ -1,27 +1,31 @@
 <body>
 <h1>AmsterdamseBrooodjes</h1>
 
-<div class="FoodSelection">
+<div id="bla">Blaaaaaaaaaaaaaaaaa</div>
+
+<div class="FoodSelection" data-name="Biefstuk" data-price="€4,50">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
     <div class="CONTENT">
     <div class="GerechtTekst">
     <h2 class="GerechtNaam">Biefstuk</h2>
+    <h2 class="GerechtPrijs">4,50</h2>
     <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
     </div>
      </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
 </div>
-<div class="FoodSelection">
+<div class="FoodSelection" data-name="Hamburger" data-price="€3.36">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
     <div class="CONTENT">
     <div class="GerechtTekst">
-    <h2 class="GerechtNaam">Biefstuk</h2>
+    <h2 class="GerechtNaam">Hamburger</h2>
+    <h2 class="GerechtPrijs">3,36</h2>
     <p class="Beschrijving">Beschrijving...</p>
     </div>
      </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
 </div>
-<div class="FoodSelection">
+<div class="FoodSelection" data-name="Frikandel" data-price="€2.23">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
     <div class="CONTENT">
     <div class="GerechtTekst">
--- view/head.php
+++ view/head.php
@@ -1,4 +1,5 @@
 <head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="javascript/scripts.js"></script>
  <link rel="stylesheet" type="text/css" href="css/style.css">
 
