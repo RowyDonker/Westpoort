58eddbbce60912e94d35168324a84a9d46d3d885
joey steffens <joeysteffens@quicknet.nl>
2018-05-25 09:57:44 +0200

    buttons producten

--- view/AmsterdamseBroodjes.php
+++ view/AmsterdamseBroodjes.php
@@ -7,7 +7,14 @@
     <div class="GerechtTekst">
     <h2 class="GerechtNaam">Biefstuk</h2>
     <h2 class="Prijs">$4.50</h2>
+      <input type="radio" name="slatomaat" value="sla">Sla<br>
+    <input type="radio" name="slatomaat" value="Tomaat">Tomaat<br>
+    <select>
+    <option value="wit brood">wit brood</option>
+    <option value="bruin brood">bruin brood</option>
+    </select>
     <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
+  
     </div>
      </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
