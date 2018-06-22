fc211b920337e0d16e2b0e3a6abbdbc86808d5bc
joey steffens <joeysteffens@quicknet.nl>
2018-06-22 15:18:34 +0200

    Fixing bugs

--- view/AmsterdamseBroodjes.php
+++ view/AmsterdamseBroodjes.php
@@ -4,7 +4,6 @@
 <div class="FoodSelection">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
     <div class="CONTENT">
-
         <div class="GerechtTekst">
         <h2 class="GerechtNaam">Biefstuk</h2>
         <h2 class="Prijs">$4.50</h2>
@@ -19,41 +18,6 @@
         </div>
     </div>
     <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText" data-name="Biefstuk" data-price="€4.50">Bestellen</p></div>
-
-    <div class="GerechtTekst">
-    <h2 class="GerechtNaam">Biefstuk</h2>
-    <h2 class="Prijs">$4.50</h2>
- 
-    <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
-        </div>
-    </div>
-    <a href="#openModal"><div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div></a>
-    <div id="openModal" class="modalDialog">
-                    <div>
-                        <a href="#close" title="Close" class="close">X</a>
-       <div class="Keuzes">
-    <input type="radio" name="slatomaat" value="sla">Sla<br>
-    <input type="radio" name="slatomaat" value="Tomaat">Tomaat<br>
-    <select>
-    <option value="wit brood">wit brood</option>
-    <option value="bruin brood">bruin brood</option>
-    </select>
-    <p class="BestelText" data-name="Biefstuk" data-price="€4.50">Bestellen</p>
-    </div>
-                        <div class="ModalContent">
-                        </div>
-                    </div>
-                </div>
-
-
-
-
-
-
-
-
-
-
 </div>
 <div class="FoodSelection" data-name="Hamburger" data-price="€3.36">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
@@ -64,7 +28,7 @@
     <p class="Beschrijving">Beschrijving...bla bla blabla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
     </div>
      </div>
-    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText">Bestellen</p></div>
+    <div class="WinkelKar"><img class="BestelKar" src="Images/shopping_cart_PNG37.png"><img class="BestelKarVol" src="Images/VolleKar.png"><p class="BestelText" data-name="Biefstuk" data-price="€4.50">Bestellen</p></div>
 </div>
 <div class="FoodSelection" data-name="Frikandel" data-price="€2.23">
     <img class="PlaatjeGerecht" src="Images/Etenmetnaam/biefstuk.jpg">
